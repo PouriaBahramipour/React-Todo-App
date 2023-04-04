003dfed80b3d870e32b28b78108015dbc2b235f8
pouria bahramipour <p@bahramipour.com>
2023-04-05 01:16:35 +0330

    config sass module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import "./styles/global.scss";
 
 const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
 
