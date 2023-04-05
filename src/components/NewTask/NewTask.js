5b3c85de54d6b774b22dbbab6ca3205c16b73031
pouria bahramipour <p@bahramipour.com>
2023-04-06 02:30:16 +0330

    edit sass

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -2,7 +2,6 @@ import React from "react";
 import Card from "../UI/Card";
 import NewTaskForm from "./NewTaskForm";
 import styles from "./NewTask.module.scss";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
