db8c558b8861d7a29e7d53d86f3757774b95a0dd
pouria bahramipour <p@bahramipour.com>
2023-04-08 01:22:47 +0330

    checkbox handler

--- src/components/Tasks/TaskItems/TaskItemLists.js
+++ src/components/Tasks/TaskItems/TaskItemLists.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./TaskItemLists.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const TaskItem = (props) => {
+  const [isCheckedChechBox, setIsCheckedChechBox] = useState(false);
+
+  const handlerDeleteTodoItem = async () => {
+    await fetch(
+      `https://todo-app-4c8 2d-default-rtdb.firebaseio.com/todo/${props.id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
+  const titleStyle = isCheckedChechBox ? styles.lineThrough : "";
+
+  const handleCheckBox = () => {
+    setIsCheckedChechBox(!isCheckedChechBox);
+  };
+
   return (
     <li key={props.id} className={styles.taskListItem}>
       <div className={styles.taskItem}>
         <div className={styles.taskTitle}>
-          <input type="checkbox" className={styles.checkbox} />
-          <p>{props.title}</p>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            onChange={handleCheckBox}
+          />
+          <p className={titleStyle}>{props.title}</p>
         </div>
         <div className={styles.taskRemove}>
-          <FontAwesomeIcon icon={faTrashCan} size="1x" />
+          <FontAwesomeIcon
+            icon={faTrashCan}
+            size="1x"
+            onClick={handlerDeleteTodoItem}
+          />
         </div>
         <div className={styles.taskCategory}>
           <p>{props.category}</p>
